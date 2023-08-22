2a21b6a01393cd360cc78fb609980d6b71d52e3e
Ronald Maduka <rolly.maduk@gmail.com>
2023-08-22 10:04:18 +0200

    first branch commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and grow with a pr
         </a>
       </header>
     </div>
