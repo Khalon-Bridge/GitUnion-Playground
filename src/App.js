f6bab60e57c0c36016c076f9bf44ad5a8c898b34
Ronald Maduka <rolly.maduk@gmail.com>
2023-08-22 10:09:05 +0200

    a commit message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editing for review <code>src/App.js</code> and save to reload.
+          Editing for review lets see <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
